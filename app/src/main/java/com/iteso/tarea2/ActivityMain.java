7f17a2f3a9670f29245b12bb7898733f5ffc8513
Renata Romero [GDL] <reromero@compusoluciones.com>
2018-09-13 16:49:27 -0500

    Modified image button and added toast message when clicked

--- app/src/main/java/com/iteso/tarea2/ActivityMain.java
+++ app/src/main/java/com/iteso/tarea2/ActivityMain.java
@@ -2,6 +2,9 @@ package com.iteso.tarea2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void displayToastMsg(View v) {
+
+        Toast toast = Toast.makeText(this, R.string.likeToast , Toast.LENGTH_SHORT);
+        toast.show();
+
     }
 }
